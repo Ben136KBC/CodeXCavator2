e7d23f5447f38822e1ff38cdc410f6af8dc40315
Ben van der Merwe <Ben136KBC@users.noreply.github.com>
2019-02-15 15:20:18 -0700

    Add files via upload

---
+++ CodeXCavator/App.xaml.cs
@@ -0,0 +1,105 @@
+﻿// Copyright 2014 Christoph Brzozowski
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using CodeXCavator.Engine.Interfaces;
+using CodeXCavator.Engine;
+
+namespace CodeXCavator
+{
+    /// <summary>
+    /// Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        private const string MESSAGE_BOX_CAPTION = "CodeXCavator - Searcher...";
+
+        internal IEnumerable<IIndex> Indexes { get; private set; }
+
+        /// <summary>
+        /// Handles the OnStartup event.
+        /// </summary>
+        /// <param name="e">Event args</param>
+        protected override void OnStartup( StartupEventArgs e )
+        {
+            base.OnStartup( e );            
+         
+            var commandLineParser = new CommandLineParser();
+            try
+            {
+                commandLineParser.ParseCommandLineArguments( e.Args );
+            }
+            catch( Exception ex )
+            {
+                MessageBox.Show( ex.Message, MESSAGE_BOX_CAPTION, MessageBoxButton.OK, MessageBoxImage.Error );
+                Application.Current.Shutdown();
+                return;
+            }
+
+            var indexes = new List<IIndex>();
+
+            // Create an index for each specified index configuration.
+            foreach( var indexConfigurationFile in commandLineParser.IndexConfigurationFiles )
+            {
+                if( !System.IO.File.Exists( indexConfigurationFile ) )
+                {
+                    MessageBox.Show( string.Format( "Index configuration file \"{0}\" does not exist!", indexConfigurationFile ), "CodeXCavator - Searcher...", MessageBoxButton.OK, MessageBoxImage.Information );
+                    continue;
+                }
+                try
+                {
+                    // Create index from the XML configuration file.
+                    IIndex index = IIndexExtensions.CreateFromXmlFile( indexConfigurationFile );
+                    if( index != null )
+                    {
+                        CodeXCavator.UI.MRUHandler.PushMRUFile(indexConfigurationFile);
+                        // Add index to list of indexes.
+                        indexes.Add( index );
+                    }
+                    else
+                    {
+                        MessageBox.Show( string.Format( "Could not initialize index from configuration file \"{0}\"!", indexConfigurationFile ), "CodeXCavator - Searcher...", MessageBoxButton.OK, MessageBoxImage.Information );
+                    }
+                }
+                catch( Exception exception )
+                {
+                    MessageBox.Show( string.Format( "Unexpected error occurred while reading index configuration file \"{0}\"!\n\n{1}", indexConfigurationFile, exception.ToString() ), "CodeXCavator - Searcher...", MessageBoxButton.OK, MessageBoxImage.Error );
+                    if( Application.Current != null )
+                        Application.Current.Shutdown();
+                }
+            }
+
+            Indexes = indexes;
+        }
+
+        /// <summary>
+        /// Handles OnExit event.
+        /// </summary>
+        /// <param name="e">Event arguments.</param>
+        protected override void OnExit( ExitEventArgs e )
+        {
+            // Dispose all indexes.
+            if( Indexes != null )
+                foreach( var index in Indexes )
+                    index.Dispose();
+            base.OnExit( e );
+        }
+
+    }
+}
---
+++ CodeXCavator/SelectCreateIndexWindow.xaml.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CodeXCavator
+{
+
+    /// <summary>
+    /// Interaction logic for SelectCreateIndexWindow.xaml
+    /// </summary>
+    public partial class SelectCreateIndexWindow : Window
+    {
+        public string IndexFileToOpen { get; set; }
+
+        public SelectCreateIndexWindow()
+        {
+            InitializeComponent();
+
+            //Now fill in some default to help users get started when they create a new index file.
+            String path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            String indexFile = path + "\\MyIndexConfig.xml";
+            String indexLocation = path + "\\MyIndex";
+
+            XMLIndexFile.Text = indexFile;
+            XMLIndexLocation.Text = indexLocation;
+            FileSourceDirectoriesRecursive.IsChecked = true;
+            FileSourceDirectoriesInclude.Text = "*.cs|*.resx|*.bas|*.bat|*.c|*.cls|*.com|*.cpp|*.cs|*.cxx|*.def|*.dsr|*.f|*.f90|*.fi|*.for|*.frm|*.h|*.hpp|*.htm|*.html|*.idl|*.inc|*.inl|*.mak|*.odl|*.rc|*.rgs|*.sql|*.vb|*.vbs|*.xml|*.xaml";
+            FileSourceDirectories.Text = ""; //path;
+
+            //Also show the last index opened.
+            string lastIndexFile = CodeXCavator.UI.MRUHandler.GetLastMRUFile(0);
+            if (lastIndexFile != null && lastIndexFile != "")
+            {
+                OpenMRU1Button.Content = "Open " + lastIndexFile;
+            }
+            else
+            {
+                OpenMRU1Button.Content = "(no recent file to show)";
+                OpenMRU1Button.IsEnabled = false;
+            }
+            lastIndexFile = CodeXCavator.UI.MRUHandler.GetLastMRUFile(1);
+            if (lastIndexFile != null && lastIndexFile != "")
+            {
+                OpenMRU2Button.Content = "Open " + lastIndexFile;
+            }
+            else
+            {
+                OpenMRU2Button.Content = "(no recent file to show)";
+                OpenMRU2Button.IsEnabled = false;
+            }
+            lastIndexFile = CodeXCavator.UI.MRUHandler.GetLastMRUFile(2);
+            if (lastIndexFile != null && lastIndexFile != "")
+            {
+                OpenMRU3Button.Content = "Open " + lastIndexFile;
+            }
+            else
+            {
+                OpenMRU3Button.Content = "(no recent file to show)";
+                OpenMRU3Button.IsEnabled = false;
+            }
+        }
+
+        private void OpenMRU(int whichMru)
+        {
+            string lastFile = CodeXCavator.UI.MRUHandler.GetLastMRUFile(whichMru);
+            IndexFileToOpen = lastFile;
+            CodeXCavator.UI.MRUHandler.PushMRUFile(lastFile);
+
+            //Now close this form, returning to our set the index
+            this.Close();
+        }
+
+        private void OpenMRU1Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenMRU(0);
+        }
+
+        private void OpenMRU2Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenMRU(1);
+        }
+
+        private void OpenMRU3Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenMRU(2);
+        }
+
+        private void BrowseExistingIndexFile_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog ofd = new System.Windows.Forms.OpenFileDialog();
+            ofd.RestoreDirectory = true;
+            ofd.Filter = "xml files (*.xml)|*.xml";
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+            ofd.ValidateNames = true;
+            ofd.DefaultExt = "xml";
+            ofd.Title = "Select Index File";
+            ofd.FilterIndex = 1;
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string indexFile = ofd.FileName;
+                IndexFileToOpen = indexFile;
+                CodeXCavator.UI.MRUHandler.PushMRUFile(indexFile);
+
+                //Now close this form, returning our set index
+                this.Close();
+            }
+        }
+
+        private void BrowseNewIndexFile_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.SaveFileDialog ofd = new System.Windows.Forms.SaveFileDialog();
+            ofd.RestoreDirectory = true;
+            ofd.Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*";
+            ofd.CheckFileExists = false;
+            ofd.CheckPathExists = true;
+            ofd.CreatePrompt = false;
+            ofd.OverwritePrompt = true;
+            ofd.ValidateNames = true;
+            ofd.DefaultExt = "xml";
+            ofd.Title = "Select Index File";
+            ofd.FilterIndex = 1;
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string indexFile = ofd.FileName;
+                XMLIndexFile.Text = indexFile;
+
+            }
+        }
+
+        private void BrowseIndexContent_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog ofd = new System.Windows.Forms.FolderBrowserDialog();
+            ofd.Description = "Folder to Contain Index Files";
+            ofd.ShowNewFolderButton = true;
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string indexLocation = ofd.SelectedPath;
+                XMLIndexLocation.Text = indexLocation;
+            }
+        }
+
+        private void BrowseCodeDirs_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog ofd = new System.Windows.Forms.FolderBrowserDialog();
+            ofd.Description = "Source Code Folder to Index";
+            ofd.ShowNewFolderButton = false;
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string path = ofd.SelectedPath;
+                String dirs = FileSourceDirectories.Text;
+                if (dirs != String.Empty)
+                {
+                    dirs = dirs + Environment.NewLine;
+                }
+                dirs = dirs + path;
+                FileSourceDirectories.Text = dirs;
+            }
+        }
+
+        /// <summary>
+        /// Create a basic configuration index file using the information provided.
+        /// </summary>
+        private void CreateIndexFile_Click(object sender, RoutedEventArgs e)
+        {
+            string indexFile = XMLIndexFile.Text;
+            string indexLocation = XMLIndexLocation.Text;
+            string fileSourceDirectories = FileSourceDirectories.Text; ;
+            if (fileSourceDirectories == "")
+            {
+                MessageBox.Show("Please select at least one source code directory first!");
+                return;
+            }
+            bool? recursive = FileSourceDirectoriesRecursive.IsChecked;
+            string includes = FileSourceDirectoriesInclude.Text;
+
+            bool xmlOk = false;
+            try
+            {
+                System.Xml.XmlWriter xmlWriter = System.Xml.XmlWriter.Create(indexFile);
+                xmlWriter.WriteStartDocument();
+                xmlWriter.WriteStartElement("Index");
+                xmlWriter.WriteAttributeString("Path", indexLocation);
+                xmlWriter.WriteStartElement("FileSources");
+
+                string[] delimiters = new string[] { ";", "\r\n" };
+                string[] dirs = fileSourceDirectories.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < dirs.Length; i++)
+                {
+                    xmlWriter.WriteStartElement("Directory");
+                    xmlWriter.WriteAttributeString("Path", dirs[i]);
+                    if (recursive == true)
+                    {
+                        xmlWriter.WriteAttributeString("Recursive", "true");
+                    }
+                    if (recursive == false)
+                    {
+                        xmlWriter.WriteAttributeString("Recursive", "false");
+                    }
+                    xmlWriter.WriteAttributeString("Include", includes);
+                    xmlWriter.WriteEndElement();
+                }
+
+                xmlWriter.WriteEndElement();
+                xmlWriter.WriteEndElement();
+
+                xmlWriter.WriteEndDocument();
+                xmlWriter.Close();
+
+                xmlOk = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            if (xmlOk)
+            {
+                xmlOk = CodeXCavator.UI.MRUHandler.UpdateIndexFile(indexFile);
+            }
+
+            if (xmlOk)
+            {
+                //Now close this form, returning set index
+                CodeXCavator.UI.MRUHandler.PushMRUFile(indexFile);
+                IndexFileToOpen = indexFile;
+                this.Close();
+            }
+        }
+    }
+}
